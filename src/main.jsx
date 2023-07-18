2b907b48c01bca4c1be061fed3a5c8ff3dd78520
rextambua <rextambua1@gmail.com>
2023-07-18 16:30:48 +0300

    add: deploy workflow

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import reportWebVitals from "./reportWebVitals";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,5 +10,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 );
 
-reportWebVitals();
 
